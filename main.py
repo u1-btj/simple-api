f9f21f3e868b1c5c6e1b9a0ad1f562ebc0654b7a
Pingky Oktiawati <pingkyoktiawati@gmail.com>
2023-12-14 16:16:54 +0700

    coba

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ login_endpoint = '/api/login'
 def home():
    return render_template('index.html', local_url=request.base_url)
 
-# @app.route('/test')
-# def test():
-#    response = requests.request("GET", f'{base_url}{user_endpoint}/2')
-#    return response.json()
+# Pingky
+@app.route('/users', methods=['GET'])
+def list():
+   response = requests.request("GET", f'{base_url}{user_endpoint}')
+   return response.json()
 
 if __name__ == '__main__':
    app.run()
