ac81850b8d9bd773e3b16e8e9fc3d8c1faa30280
devinareva17 <devinareva17@gmail.com>
2023-12-14 16:39:05 +0700

    update

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from flask import Flask
+
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 
 base_url = 'https://reqres.in'
 user_endpoint = '/api/users'
 resource_endpoint = '/api/unknown'
+register_endpoint = '/api/register'
+login_endpoint = '/api/login'
 
+@app.route('/')
+def home():
+    return render_template('index.html', local_url=request.base_url)
 
-@app.route('/devina')
-def test():
-   response = requests.request("GET", f'{base_url}{user_endpoint}/2')
+# Yuma
+@app.route('/register', methods=['POST'])
+def register():
+    payload = request.get_json()
+    response = requests.request("POST", f'{base_url}{register_endpoint}', data=payload)
+    return response.json(), response.status_code
+
+#Devina
+@app.route('/resource', methods=["GET"])
+def get_single_resource():
+   response = requests.request("GET", f'{base_url}{resource_endpoint}/2')
    return response.json()
 
 if __name__ == '__main__':
