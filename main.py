2178ae6a527eb26c9d2f5638f970f8aef75268cf
Ziayaya <ziaaa.safira1@gmail.com>
2023-12-14 18:05:18 +0700

    tambah delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import requests
 app = Flask(__name__)
 
@@ -15,10 +15,51 @@ Faiz -> Update User Put
 Yuma -> Get List Resource
 '''
 
-@app.route('/test')
-def test():
-   response = requests.request("GET", f'{base_url}{user_endpoint}/2')
+from flask import Flask, render_template, request
+import requests
+app = Flask(__name__)
+
+base_url = 'https://reqres.in'
+user_endpoint = '/api/users'
+resource_endpoint = '/api/unknown'
+register_endpoint = '/api/register'
+login_endpoint = '/api/login'
+
+@app.route('/')
+def home():
+    return render_template('index.html', local_url=request.base_url)
+
+# Yuma
+@app.route('/register', methods=['POST'])
+def register():
+    payload = request.get_json()
+    response = requests.request("POST", f'{base_url}{register_endpoint}', data=payload)
+    return response.json(), response.status_code
+
+# Faiz
+@app.route('/update', methods = ['PUT'])
+def update():
+   payload = request.get_json()
+   response = requests.request("PUT", f'{base_url}{user_endpoint}/2', data=payload)
+   return response.json(), response.status_code
+  
+# Devina
+@app.route('/resource', methods=["GET"])
+def get_single_resource():
+   response = requests.request("GET", f'{base_url}{resource_endpoint}/2')
    return response.json()
+
+# Pingky
+@app.route('/users', methods=['GET'])
+def list_users():
+   response = requests.request("GET", f'{base_url}{user_endpoint}')
+   return response.json()
+
+# Safira
+@app.route('/delete', methods=['DELETE'])
+def delete():
+   response = requests.request('DELETE', f'{base_url}{user_endpoint}/2')
+   return response.text, response.status_code
 
 if __name__ == '__main__':
    app.run()
