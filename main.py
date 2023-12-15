19a1655d8770ed0b5cf46420eb95bf9402803268
devinareva17 <152063723+devinareva17@users.noreply.github.com>
2023-12-15 20:48:06 +0700

    update get_single_resource() to db version

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import psycopg2
 import requests
+from datetime import datetime
+
 app = Flask(__name__)
+app.json.sort_keys = False
 
 base_url = 'https://reqres.in'
 user_endpoint = '/api/users'
 resource_endpoint = '/api/unknown'
 register_endpoint = '/api/register'
 login_endpoint = '/api/login'
+
+# Konfigurasi koneksi PostgreSQL
+conn = psycopg2.connect(
+    host='localhost',
+    user='postgres',
+    password='postgres',
+    database='simple_api'
+)
+
+# Membuat kursor
+cur = conn.cursor()
 
 @app.route('/')
 def home():
@@ -20,17 +35,50 @@ def register():
     return response.json(), response.status_code
 
 # Faiz
-@app.route('/update/<string:uid>', methods=['PUT'])
+@app.route('/users/<string:uid>', methods=['PUT'])
 def update(uid):
-    payload = request.get_json()
-    response = requests.request("PUT", f'{base_url}{user_endpoint}/{uid}', data=payload)
-    return response.json(), response.status_code
+    data = request.get_json() # ambil data dari body yang diberikan
+    cur.execute("SELECT * FROM users WHERE user_id = " + uid)
+    existing_user = cur.fetchone()
+
+    # jika existing_user ada, maka update value sesuai user_id
+    if existing_user:
+        # request PUT data harus utuh, jika tidak, maka tampilkan attributes yang tidak ada
+        fields = ['email', 'firstname', 'lastname', 'avatar', 'token_user']
+        missing = [x for x in fields if x not in data]
+        if missing:
+            return jsonify({'error': f'Missing attributes: {", ".join(missing)}'}), 400
+        # lakukan update sesuai dengan field body request yang diisi
+        cur.execute("UPDATE users SET email = %s, firstname = %s, lastname = %s, avatar = %s, token_user = %s WHERE user_id = %s",
+                    (data.get('email'), data.get('firstname'), data.get('lastname'), data.get('avatar'), data.get('token_user'), 
+                    uid))
+    # jika tidak ada, maka tampilkan error
+    else:
+        return jsonify({"error":"user does not exist"}), 404
+
+    waktu = str(datetime.now()) # simpan timestamp update
+    # mengambil nilai yang sudah di update sebelumnya
+    cur.execute("SELECT * FROM users WHERE user_id = " + uid)
+    updated_user = cur.fetchone()
+    # pasangkan nilai dengan nama attribut/kolomnya
+    attr = ['user_id', 'email', 'firstname', 'lastname', 'avatar', 'token_user']
+    updated_dict = dict(zip(attr, updated_user)) 
+
+    conn.commit() # commit perubahan pada database
+    return jsonify({"updated":updated_dict, "updatedAt":waktu}), 200 # tampilkan data yang sudah di update dan timestamp nya
 
 # Devina
 @app.route('/resource/<string:rid>', methods=["GET"])
 def get_single_resource(rid):
-    response = requests.request("GET", f'{base_url}{resource_endpoint}/{rid}')
-    return response.json()
+    cur.execute('select * from simple_api.public.resource where resource_id = ' + rid)
+    resource = cur.fetchone()
+    key_resource = ['id','name','year','color','pantone_value']
+    #jika resource_id tidak ada di database, return error status code 404
+    if(resource == None):
+        return {'error': 'not found'}, 404
+    else:
+        resource = dict(zip(key_resource, resource))
+        return jsonify(resource), 200
 
 # Pingky
 @app.route('/users', methods=['GET'])
@@ -41,8 +89,23 @@ def list_users():
 # Ivander
 @app.route('/users/<string:uid>', methods=["GET"])
 def get_single_user(uid):
-    response = requests.request("GET", f'{base_url}{user_endpoint}/{uid}')
-    return response.json()
+    #query ambil data user
+    user_query = 'select * from simple_api.public.users where user_id = ' + uid
+    cur.execute(user_query)
+    user = cur.fetchone()
+    # kalau user tidak ada
+    if(user==None):
+        return {'error': 'user not found'}, 404
+    else:
+        user_json = {
+            'id': user[0],
+            'email': user[1],
+            'first_name': user[2],
+            'last_name': user[3],
+            'avatar': user[4]
+        }
+        user_json = jsonify(user_json)
+        return user_json
 
 # Safira
 @app.route('/delete', methods=['DELETE'])
@@ -51,4 +114,4 @@ def delete():
     return response.text, response.status_code
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
